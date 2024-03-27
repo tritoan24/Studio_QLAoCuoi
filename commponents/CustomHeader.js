7a34490eab4ff1974985b735163dbad869d10d17
NguyenCong04 <nguyentatcong2004@gmail.com>
2024-03-27 15:50:46 +0700

    update custom header có title input search

---
+++ commponents/CustomHeader.js
@@ -0,0 +1,46 @@
+import {Image, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import InputSearch from './InputSearch';
+
+const CustomHeader = ({title, messenger}) => {
+  return (
+    <SafeAreaView>
+      <View
+        style={{
+          marginHorizontal: 15,
+          marginTop: 10,
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}>
+        <View>
+          <Text style={{color: 'black', fontFamily: 'TNBold', fontSize: 45}}>
+            {title}
+          </Text>
+          <Text
+            style={{
+              fontFamily: 'NSRegular',
+              bottom: 10,
+              left: 5,
+              fontSize: 13,
+            }}>
+            {messenger}
+          </Text>
+        </View>
+
+        <Image
+          style={{width: 50, height: 50, top: 3}}
+          source={require('../assets/image/logo3.png')}
+        />
+      </View>
+      <InputSearch
+        hint={'Tìm kiếm công việc............'}
+        icon={require('../assets/image/search.png')}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default CustomHeader;
+
+const styles = StyleSheet.create({});
