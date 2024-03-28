a5da15b31794d0647c8a94c98bc0bfc1312a5c4e
nguyenduyhaihauc <nguyenduyhaihauc@gmail.com>
2024-03-28 09:44:56 +0700

    update App.tsx

--- App.tsx
+++ App.tsx
@@ -5,7 +5,13 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Login from './navigations/Login';
 import Welcome from './navigations/Welcome';
+
 import DangKy from './navigations/DangKy';
+
+import AddNhanVien from './commponents/Screens/AddNhanVien';
+import AddNhanVienCt from './commponents/Screens/AddNhanVienCt';
+import EditNhanVien from './commponents/Screens/EditNhanVien';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -13,10 +19,16 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Welcome" component={Welcome} />
-        <Stack.Screen name="Login" component={Login} />
+        {/* <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="Login" component={Login} /> */}
         <Stack.Screen name="Main" component={MainNavigation} />
+
         <Stack.Screen name="DangKy" component={DangKy}/>
+
+        <Stack.Screen name="TaoTK" component={AddNhanVien} />
+        <Stack.Screen name="AddContunie" component={AddNhanVienCt} />
+        <Stack.Screen name="EditEmployee" component={EditNhanVien} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
